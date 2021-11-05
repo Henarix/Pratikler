ab1f533f72a3a3034819b441ddd997789d8aa381
berke peker <berke.peker@yahoo.com>
2021-11-05 12:42:37 +0300

    son

--- console-programlama/Program.cs
+++ console-programlama/Program.cs
@@ -7,6 +7,13 @@ namespace console_programlama
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz:");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Soyadınızı giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
         }
     }
 }
