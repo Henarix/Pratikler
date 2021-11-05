1d180feb8be0c41ad9651b5b3fc29ed12b32cd0b
berke peker <berke.peker@yahoo.com>
2021-11-05 12:43:00 +0300

    son1

--- console-programlama/Program.cs
+++ console-programlama/Program.cs
@@ -4,7 +4,7 @@ namespace console_programlama
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("İsminizi giriniz:");
